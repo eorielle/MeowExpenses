0a89d4d3f9887a6f3daefa8723de240d37cd4a63
Eorielle <yangmenghan@gmail.com>
2016-05-13 16:17:39 +0200

    add html pages, start working on auth method

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-var http=require('http');
+var express = require('express');
+var passport = require('passport');
+var session = require('express-session');
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser());
+
+app.use(session({ secret : 'AuFaitjesuisVRAIMENTunCHAT!'}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./app/route.js')(app,passport);
+
+app.listen(8080);
+console.log('Black magic begins...');
